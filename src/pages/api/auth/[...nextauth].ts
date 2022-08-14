62bb528ebe7ae4f7691a763181ea82bea43e141d
gustavo <gustavo.felipebc@gmail.com>
2022-08-14 18:45:12 -0300

    saving user in the bank

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
+import { fauna } from '../../../services/fauna'
+import { query as q } from 'faunadb'
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -12,5 +15,23 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+
+    },
+  }
 })
