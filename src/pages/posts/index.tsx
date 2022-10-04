a70e50f834417129f047593650f8fb1ea7cdc4a0
gustavo <gustavo.felipebc@gmail.com>
2022-10-04 20:05:37 -0300

    menu navigation

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -42,15 +42,18 @@ export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
     const response = await prismic.getByType('post', {
+        fetch: ['post.title', 'post.content'],
         pageSize: 100,
     });
+
+    console.log(response)
 
     const posts = response.results.map(post => {
         return {
             slug: post.uid,
             title: RichText.asText(post.data.title),
             excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
-            updatedData: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
                 day: '2-digit',
                 month: 'long',
                 year: 'numeric'
